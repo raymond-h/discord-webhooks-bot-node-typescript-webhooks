1eb81d4bfd0fd4ce140c7e26608855892751a774
Raymond Hammarling <raymond@hammarlings.se>
2021-12-27 01:37:13 +0100

    Try to add IPFS reference

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import fastify from 'fastify';
+import { create } from 'ipfs-http-client';
 import { helloWorld } from './hello-world';
 
+let ipfsClient: ReturnType<typeof create>;
 const server = fastify({ logger: true });
 
 server.post('/', helloWorld);
 
+server.get('/ipfs-peer-id', async () => {
+  const id = await ipfsClient.id();
+
+  return { id: id.id };
+});
+
 const start = async () => {
   try {
+    ipfsClient = create({
+      url: process.env.IPFS_API,
+    });
+
+    const id = await ipfsClient.id();
+
+    server.log.info({ id });
+
     await server.listen(3000, '0.0.0.0');
   } catch (err) {
     server.log.error(err);
