25e4f2f1b42b66c6ec92a3d5541758c33e5a283f
Raymond Hammarling <raymond@hammarlings.se>
2025-08-15 11:32:12 +0200

    Test again

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -2,6 +2,6 @@ import { WebhookResponse } from './interfaces';
 
 export async function helloWorld(): Promise<WebhookResponse> {
   return {
-    message: 'Hello world from Node and Typescript!!!'
+    message: 'Hello world from Node and Typescript!'
   };
 }
